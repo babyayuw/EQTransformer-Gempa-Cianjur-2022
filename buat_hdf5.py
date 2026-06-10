05193111308ef15cb9cd7b8fdee7c03246da0910
babyayuw <babyayuw@gmail.com>
2026-06-10 23:16:46 +0700

    Update buat_hdf5.py

--- buat_hdf5.py
+++ buat_hdf5.py
@@ -1,7 +1,7 @@
 from EQTransformer.utils.hdf5_maker import preprocessor
 
 preprocessor(
-    preproc_dir=r"D:\BMKG\EQTransformer\preprocessed_eqt_processed_hdf5",
+    preproc_dir=r"D:\BMKG\EQTransformer\preprocessed_eqt_processed_hdfs",
     mseed_dir=r"D:\BMKG\EQTransformer\preprocessed_eqt",
     stations_json=r"D:\BMKG\EQTransformer\station_list.json",
     overlap=0.3,
